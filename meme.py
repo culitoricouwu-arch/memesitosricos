92b15a358b73b3b49c7fd4ef365d1711c5d39a21
culitoricouwu-arch <culitoricouwu@gmail.com>
2026-07-01 19:05:04 -0700

    Update meme.py

--- meme.py
+++ meme.py
@@ -1,1 +1,13 @@
+import os
+import requests
 
+webhook = os.environ["WEBHOOK"]
+
+meme = requests.get("https://meme-api.com/gimme").json()
+
+requests.post(
+    webhook,
+    json={
+        "content": f"**{meme['title']}**\n{meme['url']}"
+    }
+)
