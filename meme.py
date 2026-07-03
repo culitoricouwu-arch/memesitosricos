0eb033d2e35a91f5015e940a4d493e7f615e54b6
culitoricouwu-arch <culitoricouwu@gmail.com>
2026-07-02 19:11:41 -0700

    Update meme.py

--- meme.py
+++ meme.py
@@ -1,14 +1,52 @@
 import os
+import random
 import requests
+import praw
 
-WEBHOOK = os.environ["WEBHOOK"]
-
-r = requests.post(
-    WEBHOOK,
-    json={
-        "content":"✅ Prueba exitosa desde GitHub"
-    }
+reddit = praw.Reddit(
+    client_id=os.environ["REDDIT_CLIENT_ID"],
+    client_secret=os.environ["REDDIT_CLIENT_SECRET"],
+    username=os.environ["REDDIT_USERNAME"],
+    password=os.environ["REDDIT_PASSWORD"],
+    user_agent="Discord Meme Bot"
 )
 
-print(r.status_code)
-print(r.text)
+subs = [
+    "memes",
+    "spanish",
+    "MAAU",
+    "BeelcitosMemes",
+    "dankmemes",
+    "shitposting",
+    "funny"
+]
+
+MENSAJES = [
+    "🏍️ Un motociclista dejó esto por aquí...",
+    "🍺 Ya llegó el meme rancio del rato.",
+    "☠️ La Iron Legion aprueba este meme.",
+    "🔥 Encontrado en Reddit.",
+    "🛠️ El mecánico encontró esta joyita.",
+    "😂 Meme recién robado de Reddit."
+]
+
+subreddit = reddit.subreddit(random.choice(subs))
+
+posts = []
+
+for post in subreddit.hot(limit=50):
+    if (
+        not post.stickied
+        and not post.over_18
+        and post.url.endswith((".jpg",".png",".jpeg",".gif"))
+    ):
+        posts.append(post)
+
+meme = random.choice(posts)
+
+requests.post(
+    os.environ["WEBHOOK"],
+    json={
+        "content": f"😂 **{meme.title}**\n{meme.url}"
+    }
+)
