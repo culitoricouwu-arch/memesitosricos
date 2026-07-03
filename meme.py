b0b68a76183f4a8ef05d6bd543d7ac11c7d7d0b1
culitoricouwu-arch <culitoricouwu@gmail.com>
2026-07-02 19:03:06 -0700

    Update meme.py

--- meme.py
+++ meme.py
@@ -1,101 +1,14 @@
 import os
-import random
 import requests
 
 WEBHOOK = os.environ["WEBHOOK"]
 
-HEADERS = {
-    "User-Agent": "IronLegionDiscordBot/1.0"
-}
+r = requests.post(
+    WEBHOOK,
+    json={
+        "content":"✅ Prueba exitosa desde GitHub"
+    }
+)
 
-SUBREDDITS = [
-    "MAAU",
-    "Mujico",
-    "BeelcitosMemes",
-    "memes",
-    "dankmemes",
-    "funny"
-]
-
-MENSAJES = [
-    "🏍️ Un motociclista dejó esto por aquí...",
-    "🍺 Ya llegó el meme rancio del rato.",
-    "☠️ La Iron Legion aprueba este meme.",
-    "🔥 Encontrado en Reddit.",
-    "🛠️ El mecánico encontró esta joyita.",
-    "😂 Meme recién robado de Reddit."
-]
-
-def obtener_meme():
-
-    random.shuffle(SUBREDDITS)
-
-    for sub in SUBREDDITS:
-
-        url = f"https://www.reddit.com/r/{sub}/hot.json?limit=40"
-
-        try:
-
-            data = requests.get(url, headers=HEADERS, timeout=10).json()
-
-            posts = []
-
-            for p in data["data"]["children"]:
-
-                post = p["data"]
-
-                if post["over_18"]:
-                    continue
-
-                if post["stickied"]:
-                    continue
-
-                if post["is_video"]:
-                    continue
-
-                imagen = post["url"]
-
-                if not imagen.endswith((".jpg",".jpeg",".png",".gif")):
-                    continue
-
-                posts.append(post)
-
-            if posts:
-
-                elegido = random.choice(posts)
-
-                return {
-                    "titulo": elegido["title"],
-                    "imagen": elegido["url"],
-                    "subreddit": sub
-                }
-
-        except:
-            pass
-
-    return None
-
-
-meme = obtener_meme()
-
-if meme:
-
-    mensaje = random.choice(MENSAJES)
-
-    requests.post(
-
-        WEBHOOK,
-
-        json={
-
-            "content":
-f"""{mensaje}
-
-📍 r/{meme['subreddit']}
-
-**{meme['titulo']}**
-
-{meme['imagen']}"""
-        }
-
-    )
+print(r.status_code)
+print(r.text)
