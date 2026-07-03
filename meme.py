87c3bed237167b3ffce158089f3da601d3e9569c
culitoricouwu-arch <culitoricouwu@gmail.com>
2026-07-02 19:16:31 -0700

    Update meme.py

--- meme.py
+++ meme.py
@@ -1,40 +1,13 @@
 import os
-import random
 import requests
-import praw
 
-reddit = praw.Reddit(
-    client_id=os.environ["REDDIT_CLIENT_ID"],
-    client_secret=os.environ["REDDIT_CLIENT_SECRET"],
-    username=os.environ["REDDIT_USERNAME"],
-    password=os.environ["REDDIT_PASSWORD"],
-    user_agent="Discord Meme Bot"
-)
+webhook = os.environ["WEBHOOK"]
 
-subs = [
-    "memes",
-    "dankmemes",
-    "shitposting",
-    "funny"
-]
-
-subreddit = reddit.subreddit(random.choice(subs))
-
-posts = []
-
-for post in subreddit.hot(limit=50):
-    if (
-        not post.stickied
-        and not post.over_18
-        and post.url.endswith((".jpg",".png",".jpeg",".gif"))
-    ):
-        posts.append(post)
-
-meme = random.choice(posts)
+meme = requests.get("https://meme-api.com/gimme").json()
 
 requests.post(
-    os.environ["WEBHOOK"],
+    webhook,
     json={
-        "content": f"😂 **{meme.title}**\n{meme.url}"
+        "content": f"**{meme['title']}**\n{meme['url']}"
     }
 )
